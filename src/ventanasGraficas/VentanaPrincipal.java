7837bcb27fca4c76b1668e814376a708689739bc
lucasEcheverria <lucas.echeverria@opendeusto.es>
2024-10-30 17:50:07 +0100

    Botonera del panel principal, añade botones.

--- src/ventanasGraficas/VentanaPrincipal.java
+++ src/ventanasGraficas/VentanaPrincipal.java
@@ -1,6 +1,15 @@
 package ventanasGraficas;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class VentanaPrincipal extends JFrame{
 	/**
@@ -10,12 +19,48 @@ public class VentanaPrincipal extends JFrame{
 	 */
 	
 	//Atributos
-	
+	private JPanel pBotones;
+	private JScrollPane scrollBotones;
+	private ArrayList<JButton> botonesLaterales;
+	private JButton btnProductos, btnInventario;
 	
 	public VentanaPrincipal(){
 		super();
 		setTitle("DEUSTOCOFFEE");
+		setSize(600, 400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
+		//Panel oeste
+		pBotones = new JPanel();
+		btnProductos = new JButton("PRODUCTOS");
+		btnInventario = new JButton("INVENTARIO");
+		
+		botonesLaterales = new ArrayList<JButton>();
+		botonesLaterales.add(btnProductos);
+		botonesLaterales.add(btnInventario);
+		cargarBotones(botonesLaterales);
+		
+		scrollBotones = new JScrollPane(pBotones);
+		getContentPane().add(scrollBotones, BorderLayout.WEST);
+		
+		setVisible(true);
+	}
+	
+	private void cargarBotones(ArrayList<JButton> botonesLaterales) {
+		/**
+		 * Este metodo coge los el todos los botones que hay y los mete todos en el panel de botones al lateral izquierdo de la pantalla.
+		 */
+		pBotones.setLayout(new GridLayout(botonesLaterales.size(), 1));
+		
+		for(int i=0; i<botonesLaterales.size(); i++) {
+			pBotones.add(botonesLaterales.get(i));
+		}
+		repaint();
+	}
+	
+	public static void main(String[] args) {
+		VentanaPrincipal v = new VentanaPrincipal();
 	}
 	
 }
