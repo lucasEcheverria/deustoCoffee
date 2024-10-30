3e25c8563756cfc35968a612f5cf3db92f72af53
lucasEcheverria <lucas.echeverria@opendeusto.es>
2024-10-30 17:25:07 +0100

    Eliminación de enum Tipo; Creación ventanaPrincipal.

---
+++ src/ventanasGraficas/VentanaPrincipal.java
@@ -0,0 +1,21 @@
+package ventanasGraficas;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame{
+	/**
+	 * Esta es la ventan principal en la que tienen lugar la mayoría de las interacciones con el programa.
+	 * En esta se incorporan los distintos paneles como las distintas cuentas, el inventario o la ventana de 
+	 * productos.
+	 */
+	
+	//Atributos
+	
+	
+	public VentanaPrincipal(){
+		super();
+		setTitle("DEUSTOCOFFEE");
+		
+	}
+	
+}
---
+++ src/ventanasGraficas/VentanaProductos.java
@@ -0,0 +1,10 @@
+package ventanasGraficas;
+import javax.swing.JPanel;
+
+public class VentanaProductos extends JPanel  {
+	private static final long serialVersionUID = 1L;
+	
+	public VentanaProductos() {
+		
+	}
+}
