bf131c8a25a8d98c9d2926631f9c8ac6ea4c2d5a
Paula Jiménez Urzanqui <paula.j.urzanqui@opendeusto.es>
2024-10-31 00:39:06 +0100

    Algunos elementos

--- src/ventanasGraficas/VentanaInicioSesion.java
+++ src/ventanasGraficas/VentanaInicioSesion.java
@@ -1,19 +1,48 @@
 package ventanasGraficas;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class VentanaInicioSesion extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public VentanaInicioSesion() {
 		super("Inicio de Sesión");
-		this.setSize(300, 200);
+		this.setSize(900, 600);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-	}
+		// Crear paneles
+		JPanel panelInicioSesion = new JPanel();
+		JPanel panelRegistro = new JPanel();
+		JPanel panelCampos = new JPanel();
+		panelCampos.setLayout(new GridLayout(2, 2));
 	
-	public static void main(String[] args) {
-		VentanaInicioSesion ventanaInicioSesion = new VentanaInicioSesion();
-		ventanaInicioSesion.setVisible(true);
+		// Crear componentes
+		JLabel labelInicioSesion = new JLabel("Inicio de Sesión");
+		JLabel labelRegistro = new JLabel("Registro");
+		JLabel labelUsuario = new JLabel("Usuario:");
+		JTextField campoUsuario = new JTextField(20);
+		JLabel labelContrasena = new JLabel("Contraseña:");
+		JTextField campoContrasena = new JTextField(20);
+		
+		// Configurar componentes
+		panelCampos.add(labelUsuario);
+		panelCampos.add(campoUsuario);
+		panelCampos.add(labelContrasena);
+		panelCampos.add(campoContrasena);
+		panelInicioSesion.add(labelInicioSesion, BorderLayout.NORTH);
+		panelInicioSesion.add(panelCampos, BorderLayout.SOUTH);
+		
+		
+		panelRegistro.add(labelRegistro, BorderLayout.NORTH);
+		
+		// Añadir paneles a la ventana
+		this.add(panelInicioSesion, BorderLayout.WEST);
+	    this.add(panelRegistro, BorderLayout.EAST);
 	}
+		public static void main(String[] args) {
+			VentanaInicioSesion ventanaInicioSesion = new VentanaInicioSesion();
+			ventanaInicioSesion.setVisible(true);
+		}
 }
